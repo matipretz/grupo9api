1f15fc93ff6a73a15e9920f1363801566e4ea65d
Matias Martin Murad Pretz <30426732+matipretz@users.noreply.github.com>
2024-07-05 10:48:02 -0300

    deleted:    .env.template 	modified:   app.js

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ import router from './routes/noticias.routes.js'
 const app = express()
 const PORT = process.env.PORT
 
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors())
-app.disable('x-powered-by')
+app
+  .use(express.static('public'))
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(cors())
+  .disable('x-powered-by')
+  .use('/noticias', router)
 
-app.use('/noticias', router)
-const DB_PORT = process.env.DB_PORT
-app.listen(PORT, () => {
+const DB_PORT = process.env.DB_PORT.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`)
   console.log(`DB on port ${DB_PORT}`)
   console.error(`${process.env.ENV}`)
